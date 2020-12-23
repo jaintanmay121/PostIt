ff26dd33c2c003c96c3344ef4aaf3c0e8a2745a8
Tanmay Jain <jaintanmay121@gmail.com>
2020-12-23 23:20:11 +0530

    added SSL

--- PostIt/settings.py
+++ PostIt/settings.py
@@ -19,7 +19,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG=True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -128,7 +127,7 @@ USE_TZ = True
 WHITENOISE_USE_FINDERS = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 SESSION_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
